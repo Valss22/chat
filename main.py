42c3c3215d2fc282f04a7c202897f660873495b3
Valss22 <valsshokorov@gmail.com>
2021-11-27 21:14:49 +0600

    fix origins

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from schemas.user import UserIn, UserOut
 from services.user import create_user, auth_user
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post('/user/register/', response_model=UserOut)
