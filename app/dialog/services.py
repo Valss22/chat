f3d21b42e273fd7fd7596d752599efff8095f01c
Valss22 <valsshokorov@gmail.com>
2022-03-21 19:41:42 +0600

    first

--- app/dialog/services.py
+++
@@ -1,8 +0,0 @@
-from app.settings import db
-
-
-class DialogService:
-
-    async def get_dialogs(self, count: int):
-        users = await db['users'].find().to_list(count)
-        return users
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+# type: ignore
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+from tortoise.contrib.fastapi import register_tortoise
 from app.routers import api_router
+import subprocess
+import sys
+import uvicorn
 
 
 app = FastAPI()
@@ -18,3 +22,17 @@ app.add_middleware(
 
 app.include_router(api_router)
 
+register_tortoise(
+    app,
+    db_url="sqlite://db.sqlite3",
+    modules={"models": ["app.user.model"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
+
+if __name__ == '__main__':
+    status_output: tuple[int, str] = subprocess.getstatusoutput("mypy .")
+    if status_output[0]:
+        print(status_output[1])
+    else:
+        uvicorn.run(app, host="127.0.0.1", port=8000)
